e92e0a6d6ec2e53163c793778be6c7b3e6699d3f
fasogba ayomide isaac <ayomidefasogba@gmail.com>
2025-04-15 18:39:25 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,108 @@
+// Question 1
+const checkPass = (score) => {
+    let message;
+    score > 180 ? message = "Passed" : message = "Failed"
+    console.log(message)
+}
+checkPass(200);
+
+// Question 2
+const checkEligibility = (age) => {
+    if (age > 18) {
+        console.log("You are eligible to vote")
+    } else {
+        console.log("You are not eligible")
+    }
+};
+
+checkEligibility(23)
+
+// Question 3
+function checkGrade(name, score) {
+    let grade;
+    if (score>=90) {
+        grade = "Excellent"
+    } else if (score>=75) {
+        grade = "Good"
+    } else if (score>=50) {
+        grade = "Average"
+    } else {
+        grade = "Failed"
+    }
+    console.log(`${name},Your grade is ${grade}`)
+};
+checkGrade("Ayomide", 100);
+
+// Question 4
+const checkAccess = (hasID, isAbove18) => {
+    hasID && isAbove18 ? console.log("Access Granted") : console.log("Access Denied")
+};
+checkAccess(true,true)
+
+// Question 5
+const checkStudentsGrade = () => {
+    const studentScores = [50, 80, 30, 42, 65, 70, 48];
+    for (let i = 0; i < studentScores.length; i++) {
+        if (studentScores[i]>50) {
+            console.log(`Your score is ${studentScores[i]}, you passed`)
+        } else {
+            console.log(`Your score is ${studentScores[i]}, you failed`)
+        };
+        
+    };
+}
+checkStudentsGrade();
+
+// I Tried this and it worked too
+function gradeCheck() {
+    const studentScores = [88,55, 20, 30, 42, 65, 70, 48,75,99];
+    studentScores.map((scores) => {
+        scores > 50 ? console.log("passed") : console.log("Failed")
+    });
+};
+gradeCheck();
+
+// Question 6
+
+const passCheck = (mathScore,englishScore) => {
+    if (mathScore >= 50 && englishScore >= 50) {
+        console.log("Yes")
+    } else {  
+        console.log("No")
+    };
+};
+passCheck(55,70)
+
+// Question 7
+
+function checkDetails(email,phoneNumber) {
+    if (email && phoneNumber) {
+        console.log("You have Successfully Registered")
+    } else {
+        console.log("Email and Phone Number is Required")
+    }
+}
+checkDetails('iucoueoiunl', "hkbku");
+
+
+// Question 8
+function checkInput(username,password) {
+    if (!username || !password) {
+        console.log("Invalid Input")
+    } else {
+        console.log("Go Ahead")
+    }
+};
+checkInput("Ayfash",1223)
+
+// Question 9
+const checkWorkRate = (hoursWorked) => {
+    hoursWorked >= 40 ? console.log("Full-Time") : console.log("Part-Time")
+};
+checkWorkRate(45);
+
+// Question 10
+const checkNumber = (num1, num2) => {
+    num1 > num2 ? console.log(num1) : console.log(num2);
+};
+checkNumber(25,18);
